d71968ed0b95045899a23c730201ff82fa7c57d3
kindermega <gorozhankin1@yandex.ru>
2023-11-16 16:18:47 +0300

    hw-2

---
+++ script.js
@@ -0,0 +1,78 @@
+//let a = 10;
+//alert(a);
+
+
+//let age = 2007;
+//alert(age);
+
+
+//let name = "Brendan Eich";
+//alert(name);
+
+
+//let a = 10;
+//let b = 2;
+//alert(10 + 2)
+//alert(10 - 2)
+//alert(10 * 2)
+//alert(10 / 2)
+
+
+//const result = 2 ** 5;
+//alert(result);
+
+
+//let a = 9;
+//let b = 2;
+//alert(9 % 2)
+
+
+//let num = 1;
+//num += 5;
+//num -= 3;
+//num *= 7;
+//num /= 3;
+//num += 1;
+//num -= 1;
+//alert(num);  (непонимаю правльно или нет)
+
+
+//let age = Number(prompt('Сколько Вам лет?'));
+//alert(`Вам ${age} лет`);
+
+
+//const user = {
+    //name: 'Oleg',
+    //age: 28,
+    //isAdmin: true
+//}
+
+//user['city of residence'] = true
+
+//user.age = 18;
+
+//delete user ['city of residence']
+
+
+//let info = prompt('Какую информацию хотите узнать о пользователе?', 'name', 'age');
+//console.log(user[info]);//
+
+
+//let userName = String(prompt('Как вас зовут?'));
+//alert(`Привет ${userName} !`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
